1c120f518671a072bc7dd9a95eb29b2a3ceafe7b
Chris Weed <chris@cjweed.com>
2025-11-22 15:34:06 -0700

    simple song

---
+++ images.g.ts
@@ -0,0 +1,27 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "thesong":return hex`0078000408010100001c00010a006400f4016400000400000000000000000000000000050000040600000004000125`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,1 +1,1 @@
-
+music.play(music.createSong(assets.song`thesong`), music.PlaybackMode.UntilDone)
